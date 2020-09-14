d48de4e78315f91e57976eff7cb7f78229583401
Katakana1 <40336934+Katakana1@users.noreply.github.com>
2020-09-14 14:10:58 -0400

    Update random.js

--- random.js
+++ random.js
@@ -7,5 +7,7 @@ for(var j = 0; j < 16; j++){
 for(var i = 0; i < 16; i++){
 coordX = Math.floor(OWOP.mouse.worldX/16+(i-8));
 coordY = Math.floor(OWOP.mouse.worldY/16+(j-8));
-num = [random(coordX*coordY+69)*255,random(coordX*coordY+420)*255,random(coordX*coordY+1337)*255];
-OWOP.world.setPixel(coordX,coordY,[num[0],num[1],num[2]])}}},50);
+numr = random(coordX*coordY+69)*255;
+numg = random(coordX*coordY+420)*255;
+numb = random(coordX*coordY+1337)*255;
+OWOP.world.setPixel(coordX,coordY,[numr,numg,numb])}}},50);
