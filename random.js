bea29a4945172de59e2fd5d16535ab6b94404b28
Katakana1 <40336934+Katakana1@users.noreply.github.com>
2020-09-14 19:55:17 -0400

    Update random.js

--- random.js
+++ random.js
@@ -1,10 +1,14 @@
 // Input the code into your browser's console (Ctrl-Shift-I) on ourworldofpixels.com. This creates a random pixel pattern around where the mouse is.
+function random(x){
+  return Math.abs(Math.sin(x)*10000)%1;
+}
 setInterval(function(){
-for(var j = 0; j < 16; j++){
-for(var i = 0; i < 16; i++){
-coordX = Math.floor(OWOP.mouse.worldX/16+(i-8));
-coordY = Math.floor(OWOP.mouse.worldY/16+(j-8));
-numr = Math.abs((coordX*coordY+69)*(coordX*coordY+69)*10000)%255;
-numg = Math.abs((coordX*coordY+420)*(coordX*coordY+420)*10000)%255;
-numb = Math.abs((coordX*coordY+1337)*(coordX*coordY+1337)*10000)%255;
-OWOP.world.setPixel(coordX,coordY,[numr,numg,numb])}}},50);
+  for(var j = 0; j < 16; j++){
+    for(var i = 0; i < 16; i++){
+      coordX = Math.floor(OWOP.mouse.worldX/16+(i-8));
+      coordY = Math.floor(OWOP.mouse.worldY/16+(j-8));
+      num = [Math.floor(random(coordX*coordY+69)*255),Math.floor(random(coordX*coordY+420)*255),Math.floor(random(coordX*coordY+1337)*255)];
+      OWOP.world.setPixel(coordX,coordY,[num[0],num[1],num[2]]);
+    }
+  }
+},50);
