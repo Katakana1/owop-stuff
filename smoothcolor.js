7330d6524fd3c66d80b178ff4ce3ecf4af884d1b
Katakana1 <40336934+Katakana1@users.noreply.github.com>
2020-11-10 19:57:44 -0500

    Update smoothcolor.js

--- smoothcolor.js
+++ smoothcolor.js
@@ -1,11 +1,13 @@
-// Input the code into your browser's console (Ctrl-Shift-I) on ourworldofpixels.com. This creates a relatively smooth pattern around where the mouse is.
+// Input the code into your browser's console (Ctrl-Shift-I) on ourworldofpixels.com. This creates a smooth pattern around where the mouse is.
+function smoothColor(n,d,a){
+  return (Math.sin((n+a)/d)+1)*128;
+}
 setInterval(function(){
   for(var j = 0; j < 16; j++){
     for(var i = 0; i < 16; i++){
-      coordX = Math.floor(OWOP.mouse.worldX/16+(i-8));
-      coordY = Math.floor(OWOP.mouse.worldY/16+(j-8));
-      a = (2/3)*coordX+(1/3)*coordY;
-      num = [Math.floor((Math.sin((a+420)/64)+1)*128),Math.floor((Math.sin((a+69)/192)+1)*128),Math.floor((Math.sin((a+1337)/128)+1)*128)];
+      X = Math.floor(OWOP.mouse.worldX/16+(i-8));
+      Y = Math.floor(OWOP.mouse.worldY/16+(j-8));
+      num = [Math.floor(smoothColor(X*0.666+Y*0.333,64,420)),Math.floor(smoothColor(X*0.666+Y*0.333,192,69)),Math.floor(smoothColor(X*0.666+Y*0.333,128,1337))];
       OWOP.world.setPixel(coordX,coordY,num);
     }
   }
