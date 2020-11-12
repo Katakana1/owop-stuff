b5c788158cc2afbd00243b8f773499203123121e
Katakana1 <40336934+Katakana1@users.noreply.github.com>
2020-11-12 10:35:59 -0500

    Update smoothcolor.js

--- smoothcolor.js
+++ smoothcolor.js
@@ -1,7 +1,4 @@
 // Input the code into your browser's console (Ctrl-Shift-I) on ourworldofpixels.com. This creates a smooth pattern around where the mouse is.
-function smoothColor(n){
-  return (Math.abs(n)**0.5)%1;
-}
 setInterval(function(){
   for(var j = 0; j < 16; j++){
     for(var i = 0; i < 16; i++){
